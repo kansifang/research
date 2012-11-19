fbd49ebebf178bd1a2858cfa40bce86896bfbb16
yhostc <yhostc@gmail.com>
2012-11-19 13:47:00 +0000

    update

---
+++ google-ditu/data/Untitled-1.js
@@ -0,0 +1,40 @@
+/* 点击热点，信息窗体返回信息 */
+
+{
+    "id": "A",
+    "cid": "13291683922352673571",
+    "latlng": {
+        "lat": 40.000979999999998,
+        "lng": 116.324444
+    },
+    "image": "http://maps.gstatic.cn/intl/zh-CN_cn/mapfiles/markers2/markerA.png",
+    "sprite": {
+        "width": 20,
+        "height": 34,
+        "top": 0,
+        "image": "http://maps.gstatic.cn/mapfiles/markers2/red_markers_A_J2.png"
+    },
+    "laddr": "清华园, 北京市海淀区清华西路",
+    "geocode": "CR-GMVdB6WWaFdRdYgIdXPjuBinT0gWboFbwNTEjE5riGox1uA",
+    "name": "清华园",
+    "infoWindow": {
+        "title": "\x3cb\x3e清华园\x3c/b\x3e",
+        "addressLines": ["北京市海淀区", "清华西路"],
+        "hp": {
+            "url": "/local_url?dq=%E6%B8%85%E5%8D%8E%E5%9B%AD\x26q=http://baike.baidu.com/view/32477.htm\x26oi=miw\x26sa=X\x26ct=miw_link\x26cd=1\x26cad=homepage,cid:13291683922352673571\x26ei=QuqkUMDPOLCRiQeC34DwCA\x26s=ANYYN7nCrFv7Htfgnpm4ky4duGSSFwjeng",
+            "domain": "baidu.com",
+            "actual_url": "http://baike.baidu.com/view/32477.htm"
+        },
+        "basics": "\x3cdiv transclude=\"iw\"\x3e\x3c/div\x3e",
+        "moreInfo": "更多信息",
+        "place_url": "",
+        "lbcurl": "http://www.google.com/local/add/choice?hl=zh-CN\x26gl=CN\x26latlng=13291683922352673571\x26q=%E6%B8%85%E5%8D%8E%E5%9B%AD\x26near=",
+        "link_jsaction": "app.showMoreInfo"
+    },
+    "ss": {
+        "rapenabled": true
+    },
+    "modules": ["appiw", "strr"],
+    "b_s": 2,
+    "elms": [3, 2, 6, 1, 12, 2, 13]
+}
---
+++ google-ditu/data/Untitled-3.js
@@ -0,0 +1,474 @@
+/*
+
+http://mt0.google.com/vt/pt?lyrs=m@0196000000&las=39.92205980405657;116.31520843505858;39.99795353821403;116.56971359252931&z=13&ptv=1&callback=_xdc_._bh9jz46nh
+
+*/
+
+{
+    area: [{
+        id: "vwuvttvtuwvw",//不明，非格子ID
+        zrange: [12, 12],//层级范围
+        layer: "m",//图层
+        epoch: 196000000//时代
+    }, {
+        id: "vwuvttvtuwwv",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtuwww",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwutu",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwutw",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuu",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuvu",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuvw",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuw",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwtu",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwtw",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwu",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwvu",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwvw",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwww",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvutvvv",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvttt",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvttv",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvtvt",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvtvv",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvvtt",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvvtv",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvvvt",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvvvv",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvvuutu",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvvuuut",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvvuuuu",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvwtttt",
+        zrange: [12, 12],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtuwvw",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtuwwv",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtuwww",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwutu",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwutw",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuu",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuvu",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuvw",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuw",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwtu",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwtw",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwu",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwvu",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwvw",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwww",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvutvvv",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvttt",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvttv",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvtvt",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvtvv",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvvtt",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvvtv",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvvvt",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvuvvvv",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvvuutut",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvvuutuu",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvvuuutt",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvvuuutu",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvvuuuut",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvvuuuuu",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvwttttt",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvwttttu",
+        zrange: [13, 13],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuuvv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuuvw",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuuwv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuuwwt",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuuwwv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwt",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwut",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwuut",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwuuv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwuv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwuwt",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwuwv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwwt",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwwut",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwwuv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwwv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwwwt",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwuwwwv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwut",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwuut",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwuuut",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwuuuv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwuuv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwuuwt",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwuuwv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwuv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwuwt",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwuwut",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwuwuv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwuwv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwuwwt",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }, {
+        id: "vwuvttvtwwuwwv",
+        zrange: [14, 14],
+        layer: "m",
+        epoch: 196000000
+    }]
+}
